c7a2249319e69e3fb46d8cba3153fb6e7342c01f
abdo choufani <abdo.elchoufani@gmai.com>
2019-03-04 10:47:16 +0100

    nothing

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,8 @@
 const router=require('express').Router()
-// const passport=require("passport")
 
 
 
 
-
-
-// router.get('/auth/login',(req,res)=>{
-//     res.render('login')
-// })
-
-// router.get('/auth/google/logout',(req,res)=>{
-//     res.redirect('/')
-//   })
-  
-  
-//   //  route to test for passport
-//   router.get('/auth/login/google',passport.authenticate("google",{
-//       scope:['profile']
-//   })) 
-
-
-
-//   router.get('/auth/google/redirect',passport.authenticate('google'),(req,res)=>{
-//       res.redirect('/user/profile')
-//   })
 
 
 const authCheck=(req,res,next)=>{
