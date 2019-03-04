9924d69439d1b4b57d3e86f66a95b18371be1bf3
abdo choufani <abdo.elchoufani@gmai.com>
2019-03-04 16:32:46 +0100

    test

--- socketApi.js
+++ socketApi.js
@@ -4,6 +4,7 @@ var mongoose = require("mongoose");
 var socket_io = require('socket.io');
 var io = socket_io();
 var socketApi = {};
+var Chat=require("./models/chat")
 
 socketApi.io = io;
 
@@ -17,10 +18,16 @@ io.on('connection', function(socket){
 
 
 io.on('connection', function(socket){
+  Chat.find().toArray((err,result)=>{
+    if(err){
+      throw err
+    }
+    io.sockets.emit("chat message",msg)
+  })
     socket.on('chat message', function(msg){
         debugger
+        io.sockets.emit('chat message', msg);
             console.log(msg)
-            io.sockets.emit('chat message', msg);
     });
   });
 
